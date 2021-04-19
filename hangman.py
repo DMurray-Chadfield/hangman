19b1c7786c045426559138f74d94eba155eedf5b
Dan <dan.chadfield@gmail.com>
2021-04-19 23:54:40 +0100

    Add find_all fn

--- hangman.py
+++ hangman.py
@@ -1,8 +1,19 @@
-word = input('Player one, choose a word: '
+word = input('Player one, choose a word: ')
 
 word = word.lower()
 length = len(word)
 guess = '_' * length
+
+def find_all(string, letter):
+    indices = []
+    index = string.find(letter)
+    while index != -1:
+        indices.append(index)
+        string = string.replace(letter, '-', 1)
+        index = string.find(letter)
+    return indices
+
+
 
 while guess != word:
     print(guess)
