a2641fee0c22aa0725845a1bffbce242840ff77b
Dan <dan.chadfield@gmail.com>
2021-04-19 22:02:52 +0100

    Add while loop

--- hangman.py
+++ hangman.py
@@ -1,1 +1,9 @@
-word = input('Player one, choose a word:')
+word = input('Player one, choose a word: '
+
+word = word.lower()
+length = len(word)
+guess = '_' * length
+
+while guess != word:
+    print(guess)
+    letter = input('Player two, guess a letter: ')
